62e4149ee2c1148f9c820d368b9b1fa649eaa584
Joao Lucas <palaciosoares@gmail.com>
2018-11-26 09:04:42 -0200

    Create ultrawide.sh

---
+++ ultrawide.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Change Resolution Ultrawide Linux
+gtf 2560 1080 50
+xrandr --newmode "TEST" 188.60  2560 2704 2976 3392  1080 1081 1084 1112  -HSync +Vsync
+xrandr --addmode HDMI-1 "TEST"
